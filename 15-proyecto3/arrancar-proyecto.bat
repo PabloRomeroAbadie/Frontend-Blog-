7725429079ecfe06c7bf2b7a61a142121965b290
PabloRomeroAbadie <pabloabadie1995@gmail.com>
2024-02-20 10:28:22 -0300

    primer commit, estructura general y creacion de rutas realizado

---
+++ 15-proyecto3/arrancar-proyecto.bat
@@ -0,0 +1,5 @@
+start cmd.exe /k "cd C:\Program Files\MongoDB\Server\5.0\bin && .\mongod.exe"
+start cmd.exe /k "cd C:\Users\Mariano\Desktop\BACKEND MASTER EN REACT\api-rest-node && npm start"
+start cmd.exe /k "cd C:\Users\Mariano\Desktop\FRONTEND MASTER EN REACT\15-proyecto3 && npm run dev"
+start C:\Users\Mariano\AppData\Local\MongoDBCompass\MongoDBCompass.exe 
+start C:\Users\Mariano\AppData\Local\Postman\Postman.exe 
---
+++ 15-proyecto3/src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+            &copy:Master en Javascript ES12 Y TypeScript - <a href="#">Pablo</a>
+    </footer>
+  )
+}
+
+export default Footer
