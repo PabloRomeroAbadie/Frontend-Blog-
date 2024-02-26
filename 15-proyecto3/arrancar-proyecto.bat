86e45f081c0c3a4bedf61cb3bb01ff7f232642e8
Pablo Romero Abadie <pabloabadie1995@gmail.com>
2024-02-26 17:43:43 -0300

    estructura html y estilos a los articulos

--- 15-proyecto3/arrancar-proyecto.bat
+++ 15-proyecto3/arrancar-proyecto.bat
@@ -1,5 +1,5 @@
 start cmd.exe /k "cd C:\Program Files\MongoDB\Server\5.0\bin && .\mongod.exe"
-start cmd.exe /k "cd C:\Users\Mariano\Desktop\BACKEND MASTER EN REACT\api-rest-node && npm start"
-start cmd.exe /k "cd C:\Users\Mariano\Desktop\FRONTEND MASTER EN REACT\15-proyecto3 && npm run dev"
-start C:\Users\Mariano\AppData\Local\MongoDBCompass\MongoDBCompass.exe 
-start C:\Users\Mariano\AppData\Local\Postman\Postman.exe 
+start cmd.exe /k "cd C:\Users\Leandro Romero\Desktop\mis proyectos\Backend-Blog\api-rest-node && npm start"
+start cmd.exe /k "cd C:\Users\Leandro Romero\Desktop\mis proyectos\Frontend-Blog-\15-proyecto3 && code ."
+start C:\Users\Leandro Romero\AppData\Local\MongoDBCompass 
+start C:\Users\Leandro Romero\AppData\Local\Postman 
--- 15-proyecto3/src/components/pages/Articulos.jsx
+++ 15-proyecto3/src/components/pages/Articulos.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Articulos = () => {
   return (
-    <div>Articulos</div>
+    <>
+      <article className='articulo-item'>
+        <div className='mascara'>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" alt="" />
+        </div>
+        
+        <div className='datos'>
+          <h3 className='title'>Desarrollo web</h3>
+          <p className='description'>pabloromero.es</p>
+
+          <button className='edit'>Editar</button>
+          <button className='delete'>Borrar</button>
+        </div>
+      </article>
+    </>
   )
 }
 
