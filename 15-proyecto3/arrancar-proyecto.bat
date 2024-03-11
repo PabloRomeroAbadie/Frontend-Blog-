7b121570a9a4a8459c93bf7ce2d50f648913f258
Pablo Romero Abadie <pabloabadie1995@gmail.com>
2024-03-11 11:31:46 -0300

    metodo para recorrer y mostrar articulos

--- 15-proyecto3/arrancar-proyecto.bat
+++ 15-proyecto3/arrancar-proyecto.bat
@@ -1,5 +1,3 @@
 start cmd.exe /k "cd C:\Program Files\MongoDB\Server\5.0\bin && .\mongod.exe"
 start cmd.exe /k "cd C:\Users\Leandro Romero\Desktop\mis proyectos\Backend-Blog\api-rest-node && npm start"
 start cmd.exe /k "cd C:\Users\Leandro Romero\Desktop\mis proyectos\Frontend-Blog-\15-proyecto3 && code ."
-start C:\Users\Leandro Romero\AppData\Local\MongoDBCompass 
-start C:\Users\Leandro Romero\AppData\Local\Postman 
--- 15-proyecto3/src/components/pages/Articulos.jsx
+++ 15-proyecto3/src/components/pages/Articulos.jsx
@@ -1,21 +1,49 @@
 import React from 'react'
+import { useState, useEffect } from "react";
 
 const Articulos = () => {
+
+  const [articulos, setArticulos] = useState([]);
+
+  useEffect(() => {
+    let data = [{
+      _id: 1,
+      titulo: "Titulo 1",
+      contenido: "Contenido"
+    },
+    {
+      _id: 2,
+      titulo: "Titulo 2",
+      contenido: "Contenido 2"
+    },
+    {
+      _id: 3,
+      titulo: "Titulo 3",
+      contenido: "Contenido 3"
+    }]
+
+    setArticulos(data);
+  }, [])
+
   return (
     <>
-      <article className='articulo-item'>
-        <div className='mascara'>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" alt="" />
-        </div>
-        
-        <div className='datos'>
-          <h3 className='title'>Desarrollo web</h3>
-          <p className='description'>pabloromero.es</p>
+      {articulos.map(articulo => {
+        return (
+          <article key={articulo._id} className='articulo-item'>
+            <div className='mascara'>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" alt="" />
+            </div>
 
-          <button className='edit'>Editar</button>
-          <button className='delete'>Borrar</button>
-        </div>
-      </article>
+            <div className='datos'>
+              <h3 className='title'>{articulo.titulo}</h3>
+              <p className='description'>{articulo.contenido}</p>
+
+              <button className='edit'>Editar</button>
+              <button className='delete'>Borrar</button>
+            </div>
+          </article>
+        )
+      })}
     </>
   )
 }
