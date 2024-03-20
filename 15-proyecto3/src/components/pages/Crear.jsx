251760f5f36a1bf765c5f070996ea9d2c0cd8b03
Pablo Romero Abadie <pabloabadie1995@gmail.com>
2024-03-20 12:50:19 -0300

    recogimos los datos del formulario y guardamos los mismos creando un nuevo articulo

--- 15-proyecto3/src/components/pages/Crear.jsx
+++ 15-proyecto3/src/components/pages/Crear.jsx
@@ -1,22 +1,50 @@
-import React from 'react'
+import { useState } from "react";
+import {useForm} from "../../hooks/useForm";
+import {Peticion} from "../../helpers/Peticion";
+import {Global} from "../../helpers/Global";
 
 const Crear = () => {
+
+  const {formulario, enviado, cambiado} = useForm({});
+  const [resultado, setResultado] = useState("no_enviado");
+
+  const guardarArticulo = async(e) => {
+    e.preventDefault();
+
+    //recoger datos del formulario
+    let nuevoArticulo = formulario;
+    
+    //guardar articulos en el backend
+    const {datos, cargando} = await Peticion(Global.url+"crear", "POST", nuevoArticulo);
+
+    if(datos.status === "success"){
+      setResultado("guardado");
+    }else{
+      setResultado("error")
+    }
+
+    console.log(datos);
+
+  }
+
   return (
     <div className='jumbo'>
       <h1>Crear articulo</h1>
       <p>Formulario para crear un articulo</p>
 
+      <strong>{resultado == "guardado" ? "Articulo guardado con exito" : ""}</strong>
+      <strong>{resultado == "error" ? "Los datos proporcionados son incorrectos" : ""}</strong>
       {/* Montar formulario */}
-      <form className='formulario'>
+      <form className='formulario' onSubmit={guardarArticulo}>
 
         <div className='form-group'>
           <label htmlFor='titulo'>Titulo</label>
-          <input type="text" name='titulo' />
+          <input type="text" name='titulo' onChange={cambiado} />
         </div>
 
         <div className='form-group'>
           <label htmlFor='contenido'>Contenido</label>
-          <textarea type="text" name='contenido' />
+          <textarea type="text" name='contenido' onChange={cambiado}/>
         </div>
 
         <div className='form-group'>
