ed4e534b06b15c7ed2bc16d6c18a067bc0f3d80b
Pablo Romero Abadie <pabloabadie1995@gmail.com>
2024-03-20 11:04:10 -0300

    se realizo el formulario para crear articulos y se le dio estilos

--- 15-proyecto3/src/components/pages/Crear.jsx
+++ 15-proyecto3/src/components/pages/Crear.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const Crear = () => {
   return (
-    <div>Crear</div>
+    <div className='jumbo'>
+      <h1>Crear articulo</h1>
+      <p>Formulario para crear un articulo</p>
+
+      {/* Montar formulario */}
+      <form className='formulario'>
+
+        <div className='form-group'>
+          <label htmlFor='titulo'>Titulo</label>
+          <input type="text" name='titulo' />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='contenido'>Contenido</label>
+          <textarea type="text" name='contenido' />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='file0'>Imagen</label>
+          <input type="file" name='file0' id='file' />
+        </div>
+
+        <input type="submit" value="Guardar" className='btn btn-success' />
+
+      </form>
+
+    </div>
   )
 }
 
